392745b3b01ce7a5f74be51d833b45265ce3c084
Marco Kellershoff <marco@kellershoff.net>
2024-03-25 12:06:29 +0100

    🍌 Add little gimmick footer

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
   "use strict";
   const poster = document.getElementById("poster");
   const img = document.createElement("img");
+  const footer = document.querySelector("footer");
   img.style.visibility = "hidden";
   img.src = "https://mwcdn.co/www.kellershoff.net/poster.jpg";
   img.onload = function() {
     poster.style.backgroundImage = "url(" + this.src + ")";
     poster.style.opacity = 1;
     this.parentNode.removeChild(this);
+    footer.style.opacity = 1;
   };
   document.body.appendChild(img);
 })();
