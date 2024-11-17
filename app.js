e6e7c95926146fd00756cee80dd1abf0daae6423
Marco Kellershoff <marco@kellershoff.net>
2024-11-17 17:45:14 +0100

    fix(js): change cdn domain

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
   const img = document.createElement("img");
   const footer = document.querySelector("footer");
   img.style.visibility = "hidden";
-  img.src = "https://mwcdn.co/www.kellershoff.net/poster.jpg";
+  img.src = "https://yaliyomo.com/www.kellershoff.net/poster.jpg";
   img.onload = function() {
     poster.style.backgroundImage = "url(" + this.src + ")";
     poster.style.opacity = 1;
